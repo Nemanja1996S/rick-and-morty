b3c4234eeb2f436454ff3d9eb1be21d2102fa57f
Nemanja1996S <savic1996.neca@gmail.com>
2025-06-11 19:17:59 +0200

    Toolbar component

---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,17 @@
+import type { ChangeEvent } from 'react'
+import img from '../assets/rick_and_morty.webp'
+
+interface Props {
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void
+}
+
+const Toolbar = ( {onChange} : Props) => {
+  return (
+    <div className='flex flex-row justify-between align-middle min-h-40px px-4' >
+        <img src={img} alt="rick_and_morty_icon" className='h-10 w-10 mr-1' />
+        <input type="text" placeholder="Search" className="input input-neutral max-w-36 sm:max-w-none" onChange={onChange} />
+    </div>
+  )
+}
+
+export default Toolbar
